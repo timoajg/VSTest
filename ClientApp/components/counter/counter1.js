ce890e61c943a7cbec2f998a78b1321a92b4cf4c
Timo Gunst <timo.gunst@gunstconsulting.fi>
2018-02-20 11:12:37 +0200

    Add project files.

---
+++ ClientApp/components/counter/counter1.js
@@ -0,0 +1,15 @@
+﻿import Vue from 'vue';
+
+export default {
+    data: function () {
+        return {
+            currentcount: 0
+        }
+    },
+    methods: {        
+        incrementCounter: function ()
+        {
+            this.currentcount += 2;
+        },
+    }
+}
